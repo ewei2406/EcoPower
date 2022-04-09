3e3f498aebace1de84c832a3dd3f3d5cb5f889bb
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2022-04-09 14:39:57 -0400

    products created

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,62 @@
 const express = require('express')
 const app = express()
 
+var products = [];
+storeData();
+function storeData() {
+    var product_assoc = [
+        "Bar soap",
+        "Bar soap",
+        "Sunscreen",
+        "Sunscreen",
+        "Sunscreen",
+        "Bag",
+        "Bag"
+    ]
+    var names = [
+        "Lush: Goddess Bar Soap", 
+        "Fifth Origins:’ Dream Pack", 
+        "Bolden USA: SPF 30 Brightening Moisturizer",
+        "Kinlo: Golden Rays Golden Rays Sunscreen SPF 50+ Melanated Tint",
+        "Goddess Garden Organics: Daily SPF 30 Mineral Sunscreen Lotion",
+        "Stasher: Stand-Up Mini Bag",
+        "Lunchskins: Snack Bag"
+    ];
+    var scores = [
+        95,
+        90,
+        77,
+        70,
+        97,
+        89,
+        93
+    ]
+    var prices = [
+        12.50,
+        8.25,
+        28.00,
+        19.99,
+        5.99,
+        17.99,
+        5.99,
+    ]
+    for(var i = 0; i < product_assoc.length; i++) {
+        products.push({
+            product_assoc: product_assoc[i],
+            name: names[i],
+            score: scores[i],
+            price: prices[i]
+        });
+    }
+
+} 
 
 app.get('/search', (req, res) => {
-    res.send("AWODUYAWDAWD")
+    var search_res = req.params.query;
+    products.filter(obj => {
+        return obj.product_assoc == search_res;
+    })
+    res.json(JSON.stringify(products))
 })
 
 
